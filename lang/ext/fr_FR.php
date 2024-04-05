0b0cbcfabd6ef8fab8c9e1223b866f52aa99b589
Chris Taylor <chris@binnovative.co.uk>
2024-04-05 17:16:57 +0100

    v1.0 - fork of UserGuide2 + features

---
+++ lang/ext/fr_FR.php
@@ -0,0 +1,69 @@
+<?php  // Traduction check 04/03/24
+#This file is part of CMS Made Simple module: UserGuide
+#Copyright (C) 2024 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+#Refer to license and other details at the top of file UserGuide.module.php
+#More info at http://dev.cmsmadesimple.org/projects/userguide
+
+$lang['friendlyname'] = 'Guide de l\’utilisateur';
+$lang['admindescription'] = 'Pour l\’affichage et l\’édition d’un guide d\’utilisation CMS personnalisable';
+$lang['need_permission'] = 'Vous avez besoin d’une autorisation pour utiliser ce module';
+
+// Install
+$lang['ask_uninstall'] = 'Voulez-vous vraiment désinstaller le module UserGuide ? Toutes les informations seront définitivement supprimées.';
+
+// User Guide pages
+$lang['smarty_error'] = 'Cette page peut contenir une balise Smarty qui ne s\'affiche pas correctement. Veuillez ajouter les balises {literal}{/literal}, les supprimer ou désactiver le traitement.';
+$lang['admin_only_visible'] = 'visible uniquement par les utilisateurs administrateurs';
+
+// Pages Tab
+$lang['tab_pages'] = 'Pages';
+$lang['submit'] = 'Valider';
+$lang['cancel'] = 'annuler';
+$lang['save_options'] = 'Sauvegarder les options';
+$lang['apply'] = 'Appliquer';
+$lang['add_item'] = 'Créer une nouvelle page du guide utilisateur';
+$lang['edit_item'] = 'Editer la page du guide utilisateur';
+$lang['item_saved'] = 'La page du guide de l\'utilisateur est maintenant sauvegardée';
+$lang['item_notsaved'] = 'Cette page du guide utilisateur n\a pas été enregistrée';
+$lang['edit'] = 'Editer a page du guide utilisateur';
+$lang['delete'] = 'Supprimer cette page du guide utilisateur';
+$lang['confirm_delete'] = 'Êtes-vous sûr(e) de vouloir supprimer ce guide utilisateur ?';
+$lang['item_deleted'] = 'Cette page du guide utilisateur est maintenant supprimée';
+$lang['order_error'] = 'Erreur trouvée et corrigée dans l\'ordre des pages - veuillez réessayer';
+$lang['view_user_guide'] = 'Voir le guide de l\'utilisateur';
+
+
+// Options Tab
+$lang['tab_options'] = 'Options';
+$lang['title_customModuleName'] = 'Nom de module personnalisé&nbsp;';
+$lang['title_adminSection'] = 'Section d\'administration du module&nbsp;';
+$lang['title_customCSS'] = 'CSS personnalisé&nbsp;';
+$lang['text_customCSS'] = 'Pour ajouter un style personnalisé aux pages du guide de l\'utilisateur, créez un fichier \'<site-root>/assets/module_custom/UserGuide/custom.css\'. Vous pouvez utiliser la classe .guide pour le contenu du guide de l\'utilisateur.';
+$lang['title_useSmarty'] = 'Utiliser Smarty&nbsp;';
+$lang['help_useSmarty'] = 'Traiter toutes les pages du guide de l\'utilisateur avec Smarty avant de les afficher ? Si cette option est activée, vous devrez utiliser des balises {literal} autour des accolades { & } dans le contenu..';
+$lang['settings_saved'] = 'Vos options ont été sauvegardées.';
+$lang['saved_and_imported'] = 'Options sauvegardées et importées des pages du guide de l\'utilisateurs.';
+$lang['import_completed'] = 'Pages Guide de l\'utilisateur et paramètres importés.';
+$lang['file_error'] = 'Erreur de nom de fichier - non sélectionné.';
+$lang['import_error'] = 'Erreur importation - le fichier n\'a pas été importé correctement.'; 
+$lang['title_export'] = 'Exporter le contenu en XML';
+$lang['title_import'] = 'Importer le contenu depuis XML';
+$lang['title_import_export'] = 'Importer et Exporter le contenu';
+$lang['xml_export'] = 'Export XML ';
+$lang['xml_import'] = 'Importer le contenu du guide';
+$lang['title_exportImageFolder'] = 'Exporter un dossier Image&nbsp;'; 
+$lang['text_exportImageFolder'] = 'Emplacement des images du guide de l\'utilisateur à inclure dans l\'exportation, par exemple "images/userguide/". L\'emplacement est relatif à uploads_url. Laisser vide pour ne pas exporter les images.';
+$lang['title_import_UsersGuide'] = 'Importer depuis le module UsersGuide';
+$lang['import_UsersGuide'] = 'Importer le contenu et les paramètres';
+$lang['text_UsersGuide'] = 'depuis le module UsersGuide';
+$lang['none'] = 'aucun';
+
+// Edit User Guide Page
+$lang['error_title_empty'] = 'Un titre est requis'; 
+$lang['title_title'] = 'Titre&nbsp;';
+$lang['title_active'] = 'Actif&nbsp;';
+$lang['title_content'] = 'Contenu&nbsp;';
+$lang['title_admin'] = 'Administrateurs uniquement&nbsp;'; 
+$lang['prompt_admin'] = 'visible uniquement par les utilisateurs de l\'administration';
+
+
